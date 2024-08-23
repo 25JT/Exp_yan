4f2a27ca793a9aff6e250b6f632f24952b65d5a7
25JT <jhonierlucumimeza@gmial.com>
2024-08-23 16:14:38 -0500

    Ver 0.0.3 EXP_yanbal

--- scripts/Star.js
+++ scripts/Star.js
@@ -1,2 +1,32 @@
+// Animaciones con Anime.js
+document.addEventListener('DOMContentLoaded', function() {
+    anime({
+        targets: '.etapa .card',
+        translateY: [50, 0],
+        opacity: [0, 1],
+        duration: 1500,
+        delay: anime.stagger(300) // delay entre las tarjetas
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Animaciones para la sección de beneficios
+    anime({
+        targets: '.benefits-item',
+        translateY: [50, 0],
+        opacity: [0, 1],
+        duration: 1500,
+        delay: anime.stagger(200) // delay entre los items de beneficios
+    });
+
+    // Animaciones para la Ruta de Sueños
+    anime({
+        targets: '.etapa .card',
+        translateY: [50, 0],
+        opacity: [0, 1],
+        duration: 1500,
+        delay: anime.stagger(300) // delay entre las tarjetas
+    });
+});
 
 
